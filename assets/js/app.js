c9841a34f9c69fbc1f1dfac3cee74bdbb1d3aa82
joshinarendra712 <105208879+joshinarendra712@users.noreply.github.com>
2022-09-28 20:13:49 +0530

    first layout
    bootstrap layout

---
+++ assets/js/app.js
@@ -0,0 +1,40 @@
+$('.carousel').carousel({
+    interval: 2000,
+    pause:false
+  })
+
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    },
+    navText:['<i class="fa-solid fa-arrow-left"></i>','<i class="fa-solid fa-arrow-right"></i>']
+})
+
+// $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     nav:true,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:3
+//         },
+//         1000:{
+//             items:5
+//         }
+//     }
+// })
